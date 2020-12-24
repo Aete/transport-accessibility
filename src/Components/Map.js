cc00b1f5fe3347441e946f496a6e3115b7b02b9e
Seunggyun Han <hsg8234@naver.com>
2020-12-24 19:04:40 +0900

    add count_bus_stop attribute to the geojson file
    - change the url of the geojson file in 'Map.js'
    - move notebooks to a 'notebooks' folder
    - add a notebook for count bus stops around each buildings

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -9,12 +9,13 @@ mapboxgl.accessToken =
 function Map() {
   const mapContainerRef = useRef(null);
   const [map, setMap] = useState(null);
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/sghan/ck1ljdcmy16fc1cpg0f4qh3wu',
       center: [127.06243582034075, 37.49804469532547],
-      zoom: 12.8,
+      zoom: 12.7,
       attributionControl: false,
     }).addControl(
       new mapboxgl.AttributionControl({
@@ -25,7 +26,7 @@ function Map() {
       map.addSource('buildings', {
         type: 'geojson',
         data:
-          'https://raw.githubusercontent.com/Aete/mapbox-practice/master/src/data/gangnamGu-building.geojson',
+          'https://raw.githubusercontent.com/Aete/transport-accessibility/master/src/data/gangnamGu-building-residential.geojson',
       });
 
       map.addLayer({
@@ -38,6 +39,9 @@ function Map() {
           'fill-opacity': 0.7,
         },
       });
+
+      "paint": {
+        
 
       setMap(map);
     });
