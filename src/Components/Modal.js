a35a5a4be057b3fd8a19cc3e911f3f8b4ae1da1d
Seunggyun Han <hsg8234@naver.com>
2021-02-01 21:34:03 +0900

    create title and project description in the modal element

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -4,24 +4,31 @@ export default function Modal({ handleModal }) {
   return (
     <article className="Modal">
       <header className="Modal__header">
-        <h1>Title</h1>
+        <h1>Transportation Accessibility Dashboard</h1>
         <button onTouchStart={handleModal} onClick={handleModal}>
           close
         </button>
       </header>
       <main>
+        <p className="author">
+          2021. 12 ~ present, Seunggyun Han & Woowon Chung
+        </p>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quos
-          quibusdam excepturi maiores recusandae ut ipsum libero illum provident
-          odio ducimus alias labore ad necessitatibus soluta, rem vel cum
-          veritatis quam dignissimos animi. Alias porro deserunt, sed asperiores
-          laborum modi dolorem. Odio praesentium autem, error nobis vitae sunt
-          velit necessitatibus fuga ut earum accusamus iusto, ipsam repudiandae
-          ratione cupiditate ullam ea. Nostrum, quaerat. Quod architecto eveniet
-          sed similique accusamus libero vitae optio magni natus aut reiciendis
-          placeat provident id neque nisi fuga necessitatibus voluptates laborum
-          molestias veniam, maiores qui, minima repellat rem? Ipsam similique
-          impedit praesentium? Inventore ducimus et deserunt!
+          <strong>In Seoul</strong>, where personal car ownership is low and
+          dependence on public transportation is very high, accessibility to
+          public transportation is a very important factor when choosing a place
+          to work or live. Therefore, in the places with good access to public
+          transport, the most expensive and large-scale apartment complexes have
+          been built, and in places with relatively poor accessibility, small
+          and inexpensive multi-family houses are located.
+        </p>
+        <p>
+          In this study, targeting the Gangnam district, we investigated the
+          areas where actual public transportation (bicycle, subway, bus)
+          accessibility is weak and where it is good. In addition, by analyzing
+          the types of housing in each surveyed region, we tried to find a
+          district where redevelopment would be possible and a place where a new
+          public transport plan is needed.
         </p>
       </main>
     </article>
